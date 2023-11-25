a13c5d5ea1bb166633755f50e1b165d389cf847f
ammarrqureshi <mamqureshi786@gmail.com>
2023-11-25 13:40:30 +0500

    typescript files updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Note from "./components/note/Note";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        WEETASK
+       <Note/>
       </header>
     </div>
   );
