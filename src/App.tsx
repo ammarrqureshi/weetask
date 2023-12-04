7c6d6cb41635d806ffefd1425145c2fd0a4b3908
ammarrqureshi <mamqureshi786@gmail.com>
2023-12-04 20:21:58 +0500

    errors fixed in keypress

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import { Task } from 'components/task/Task';
-import { Button , Input } from 'components/UI'
-import { KeyboardEvent, useEffect } from 'react';
+import { Task } from "components/task/Task";
+import { Button, Input } from "components/UI";
+import { KeyboardEvent, useEffect } from "react";
 
-const App: React.FC= () => {
-  
-  const handleKeyDown = (event: KeyboardEvent) => {
-    if (event.code === 'ArrowUp') {
-      // do your stuff
-      alert('ArrowUp key pressed!');
-    }
-  };
-
+const App: React.FC = () => {
   useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown);
+    const handleKeyDown = (event: any) => {
+      if (event.code === "ArrowUp") {
+        event.preventDefault();
+        alert("ArrowUp key pressed!");
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
 
     return () => {
-      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener("keydown", handleKeyDown);
     };
   }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
-       WEETASK
-       <Task/>
-       <Input/>
-       <button >click to log</button>
-       {/* <Button value='hello' >Hello</Button> */}
+        WEETASK
+        <Task />
+        <Input />
+        <button>click to log</button>
+        {/* <Button value='hello' >Hello</Button> */}
       </header>
     </div>
   );
-}
+};
 
 export default App;
