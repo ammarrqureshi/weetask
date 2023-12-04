e1f71b3325435eaf909421c1cc8a97c40324d252
ammarrqureshi <mamqureshi786@gmail.com>
2023-12-04 19:52:38 +0500

    keydown press event added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { Task } from 'components/task/Task';
 import { Button , Input } from 'components/UI'
+import { SyntheticEvent } from 'react';
 
 function App() {
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.code === 'ArrowUp') {
+      // do your stuff
+      console.log('ArrowUp key pressed!');
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
        WEETASK
        <Task/>
        <Input placeholder="placeholder here"/>
-       <Button value='hello' >Hello</Button>
+       <button onKeyDown={handleKeyDown}>click to log</button>
+       {/* <Button value='hello' >Hello</Button> */}
       </header>
     </div>
   );
