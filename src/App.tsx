69a84b9543892c2d16e48cdd7c2e27adc0acf004
ammarrqureshi <mamqureshi786@gmail.com>
2023-12-04 20:32:10 +0500

    arrow keys functions added

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,35 @@ import { Button, Input } from "components/UI";
 import { KeyboardEvent, useEffect } from "react";
 
 const App: React.FC = () => {
-  useEffect(() => {
-    const handleKeyDown = (event: any) => {
-      if (event.code === "ArrowUp") {
-        event.preventDefault();
-        alert("ArrowUp key pressed!");
-      }
-    };
+  const handleKeyDown = (event: any) => {
+    switch (event.code) {
+      case "ArrowUp":
+        
+       console.log(" Mario moves up");
+        break;
+      case "ArrowDown":
+        alert(" Mario moves down");
+        break;
+      case "ArrowLeft":
+        alert(" Mario moves left");
+        break;
+      case "ArrowRight":
+        alert(" Mario moves right");
+        break;
 
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
     document.addEventListener("keydown", handleKeyDown);
 
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
     };
   }, []);
-  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -25,6 +39,10 @@ const App: React.FC = () => {
         <Task />
         <Input />
         <button>click to log</button>
+        <div
+          id="box"
+          style={{ background: "red", width: "10px", height: "10px" }}
+        ></div>
         {/* <Button value='hello' >Hello</Button> */}
       </header>
     </div>
