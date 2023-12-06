f08611fbcfcfa4be3efdd84ef36188340567bc26
ammarrqureshi <mamqureshi786@gmail.com>
2023-12-06 19:55:14 +0500

    runtime errors in context file

---
+++ src/types/types.tasks.ts
@@ -0,0 +1,13 @@
+export type TaskType = {
+  id: number;
+  text: string;
+  priority?: "high" | "medium" | "low" | string | undefined;
+  status?: "progress" | "pending" | "completed";
+  isComplete: boolean;
+  created_at: Date;
+};
+
+export interface TaskContextType {
+  tasks: TaskType[];
+  addTask: (task: TaskType) => void;
+}
