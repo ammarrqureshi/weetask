7c8a568d262fc6f75922eb5be2e7d26ebae174e4
ammarrqureshi <mamqureshi786@gmail.com>
2023-12-12 21:39:43 +0500

    final today commit

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -22,31 +22,13 @@ export const Task: React.FC<taskProps> = (
   const taskClickHandler = () => {
     checkTask(task.id);
   };
-
-  // const [color, setColor] = useState("green");
-  // useEffect(() => {
-  //   switch (task.priority) {
-  //     case "high":
-  //       setColor("teal");
-  //       break;
-  //     case "medium":
-  //       setColor("teal");
-  //       break;
-  //     case "low":
-  //       setColor("orange");
-  //       break;
-
-  //     default:
-  //       setColor("green");
-  //   }
-  // }, [task.priority]);
   const getColor = () => {
     
     switch (task.priority) {
       case "high":
-        return "text-rose-500 border-rose-500";
+        return "text-red-400 border-red-400";
       case "medium":
-        return "text-teal-300 border-teal-300 ";
+        return "text-teal-400 border-teal-400 ";
         case "low":
           return "text-yellow-300 border-yellow-300 ";
     
@@ -59,11 +41,11 @@ export const Task: React.FC<taskProps> = (
 
   return (
     <div
-      className={`task  cursor-pointer m-8 py-4 px-5 flex inline-flex items-center gap-4 justify-between shadow-inner  bg-slate-800  border-slate-700 border  dark:text-slate-300 rounded-md hover:shadow-lg 
+      className={` duration-500 cursor-pointer m-8 py-4 px-5 flex inline-flex items-center gap-4 justify-between shadow-inner  bg-slate-800  border-slate-700 border  dark:text-slate-300 rounded-md hover:shadow-lg 
       )}`}
     >
       {task.isComplete ? (
-        <Icon onClick={taskClickHandler} className="w-6" name="checkIcon" />
+        <Icon onClick={taskClickHandler} className="w-6 " name="checkIcon" />
       ) : (
         <Icon onClick={taskClickHandler} name="circleIcon" className="w-6" />
       )}
