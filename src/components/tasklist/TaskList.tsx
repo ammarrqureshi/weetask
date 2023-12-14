4fae60ef2ec0ac769077e0a7a221ab33a6119887
ammarrqureshi <mamqureshi786@gmail.com>
2023-12-14 15:41:14 +0500

    empty state for tasklist

--- src/components/tasklist/TaskList.tsx
+++ src/components/tasklist/TaskList.tsx
@@ -5,59 +5,72 @@ import { TaskContextType } from "../../types/types.tasks";
 import { DragEvent } from "react";
 
 import { FormProvider } from "../../contexts/FormContext";
+import { Button } from "../UI";
 export const TaskList = () => {
   const [dragItemIndex, setDragItemIndex] = useState<number>(0);
   const [dragOverItemIndex, setDragOverItemIndex] = useState<number>(0);
 
-  const handleDragStart = (event:DragEvent<HTMLDivElement>, index: number) => {
+  const handleDragStart = (event: DragEvent<HTMLDivElement>, index: number) => {
     console.log(index);
     setDragItemIndex(index);
-
-
   };
-  const handleDragOver = (ev:DragEvent<HTMLDivElement>) => {
+  const handleDragOver = (ev: DragEvent<HTMLDivElement>) => {
     ev.preventDefault();
-  }
+  };
   const handleDrop = (index: number) => {
-    console.log(index)
+    console.log(index);
     const _tasks = [...tasks];
-    const [dragItem]=  _tasks.splice(dragItemIndex , 1);
-    _tasks.splice(dragOverItemIndex , 0, dragItem);
+    const [dragItem] = _tasks.splice(dragItemIndex, 1);
+    _tasks.splice(dragOverItemIndex, 0, dragItem);
     setTasks(_tasks);
- 
   };
 
   const handleDragEnter = (index: number) => {
-    console.log(index)
+    console.log(index);
     setDragOverItemIndex(index);
-  
-   
   };
 
- 
   const { tasks, setTasks, checkTask, deleteTask } = useContext(
     TaskContext
   ) as TaskContextType;
   return (
-    <div className="box-border overflow-hidden max-w-lg  flex flex-col gap-4 justify-center items-center flex-wrap py-4 pb-8 border-t border-slate-800">
-      {tasks.map((task, index) => (
-        <FormProvider>
-          {" "}
-          <Task
-          className=" bg-orange-500"
-            onDragStart={(e) => handleDragStart(e,index)}
-            onDragOver={handleDragOver}
-            onDrop={() => handleDrop(index)}
-            onDragEnter={() => handleDragEnter(index)}
-          
-            key={index}
-            task={task}
-            checkTask={checkTask}
-            deleteTask={deleteTask}
-            {...task}
-          />{" "}
-        </FormProvider>
-      ))}
+    <div className="box-border overflow-hidden max-w-lg relative flex flex-col gap-4 justify-center items-center flex-wrap py-4 pb-8 border-t border-slate-800">
+      {tasks.length > 0 ? (
+        <>
+          {tasks.map((task, index) => (
+            <FormProvider>
+              {" "}
+              <Task
+                className=" bg-orange-500"
+                onDragStart={(e) => handleDragStart(e, index)}
+                onDragOver={handleDragOver}
+                onDrop={() => handleDrop(index)}
+                onDragEnter={() => handleDragEnter(index)}
+                key={index}
+                task={task}
+                checkTask={checkTask}
+                deleteTask={deleteTask}
+                {...task}
+              />{" "}
+            </FormProvider>
+          ))}
+
+          <div
+            id="tasks-count"
+            className="text-xs opacity-30 absolute bottom-0 left-0 border-t border-inherit px-4 mt-4"
+          >
+            {" "}
+            {tasks.length} tasks left
+          </div>
+        </>
+      ) : (
+        <div
+          id="list-empty-state"
+          className="text-xs opacity-30 absolute bottom-0 left-0 border-t border-inherit px-4 mt-4"
+        >
+          Hurrah! All Finished!!!
+        </div>
+      )}
     </div>
   );
 };
