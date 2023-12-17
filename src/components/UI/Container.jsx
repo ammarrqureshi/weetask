f1ff7fb032e6f49a06067a1ec5f6fe16fc8afc52
ammarrqureshi <mamqureshi786@gmail.com>
2023-12-17 19:10:51 +0500

    container component styles fixed

--- src/components/UI/Container.jsx
+++ src/components/UI/Container.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Container = ({children}) => {
   return (
-    <div className='w-[80%] m-auto overflow-auto'>{children}</div>
+    <div className='w-[80%] m-auto overflow-auto max-sm:w-[95%]'>{children}</div>
   )
 }
