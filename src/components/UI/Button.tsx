39220d5e2e7b68033f5978e8010ac2b05e9271ba
ammarrqureshi <mamqureshi786@gmail.com>
2024-01-17 16:15:45 +0500

    feat: multiple btns to ad task

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -27,7 +27,7 @@ export const Button = ({ children, Type, className, ...rest }: ButtonType) => {
         Type === "icon"
           ? `p-1 hover:bg-orange-700 rounded-lg max-sm:p-0`
           : `px-4 py-2 rounded-full text-slate-100 ${getType()} bg-${getType()}-600 hover:bg-${getType()}-500`
-      } drop-shadow-xl  disabled:bg-gray-800 disabled:text-gray-900`}
+      } flex gap-2 justify-center items-center drop-shadow-xl  disabled:bg-gray-800 disabled:text-gray-900`}
       {...rest}
     >
       {children}
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { FormContext } from "../../contexts/FormContext";
 import { FormContextType } from "../../types/types.tasks";
-import { Button } from "../UI";
+import { Button, Icon } from "../UI";
 import { AddForm } from "../taskform/AddForm";
 import { TaskProvider } from "../../contexts/TaskContext";
 
@@ -12,9 +12,23 @@ export const Sidebar = () => {
 
   return (
     <div className="border-r p-2 border-slate-500/20 min-w-[250px] ">
-      <Button Type="primary" onClick={() => setIsEditing(true)}>
-        Add Task
+      <Button
+        className="fixed bottom-8 right-4"
+        Type="primary"
+        onClick={() => setIsEditing(true)}
+      >
+        + Add Task
       </Button>
+      <Button
+        className="text-orange-500 hover:text-slate-300"
+        Type=""
+        type="button"
+        onClick={() => setIsEditing(true)}
+      >
+        <Icon name="plusCircleIcon" width="4px" height="4px" />
+        <p className="">Add Task</p>
+      </Button>
+
       {isEditing ? <AddForm /> : null}
     </div>
   );
