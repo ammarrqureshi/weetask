9599be051074f6c7e6f65324357a5dd088664a4c
ammarrqureshi <mamqureshi786@gmail.com>
2024-01-17 16:18:11 +0500

    feat: multiple btns to ad task

--- src/components/UI/Tooltip.tsx
+++ src/components/UI/Tooltip.tsx
@@ -8,7 +8,7 @@ type TooltipProps = {
 export const Tooltip = ({ children, className }: TooltipProps) => {
   return (
     <span
-      className={` hidden bg-slate-800 text-slate-300 rounded-md border border-slate-700 text-xs group-hover:inline-block py-1 px-3  absolute top-full mt-2 left-1/2 transform -translate-x-1/2`}
+      className={`z-[999] hidden bg-slate-800 text-slate-300 rounded-md border border-slate-700 text-xs group-hover:inline-block py-1 px-3  absolute top-full mt-2 left-1/2 transform -translate-x-1/2`}
     >
       {children}
     </span>
