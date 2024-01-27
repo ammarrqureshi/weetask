101dbffa78def9d118a3386c2777c03dd16609ec
ammarrqureshi <mamqureshi786@gmail.com>
2024-01-27 22:07:47 +0500

    feat: loggedin state added

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,31 @@ import { Container } from "./components/UI/Container";
 import { Navbar } from "./components/Navbar";
 import { Sidebar } from "./components/sidebar/Sidebar";
 
-import { Register } from "./components/auth/Register";
 import { GoogleAuth } from "./components/auth/GoogleAuth";
+import { useState } from "react";
+import { Register } from "./pages/Register";
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div className="text-slate-200 h-full w-full ">
       <header className="w-full">
         <Navbar></Navbar>
       </header>
       <main className="h-full flex w-full">
-        <FormProvider>
-          <Sidebar />
-        </FormProvider>
-        <div className="box-border w-full">
-          <Container>
-            <TaskList></TaskList>
-          </Container>
-        </div>{" "}
+        {isLoggedIn ? (
+          <Register />
+        ) : (
+          <>
+            <FormProvider>
+              <Sidebar />
+            </FormProvider>
+            <div className="box-border w-full">
+              <Container>
+                <TaskList></TaskList>
+              </Container>
+            </div>{" "}
+          </>
+        )}{" "}
       </main>
     </div>
   );
