dab23c469d935135992a6b7179fcb585fc035b00
ammarrqureshi <mamqureshi786@gmail.com>
2024-01-27 22:11:15 +0500

    feat: register form updated

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,11 +1,17 @@
+import { Input } from "../components/UI";
 import { GoogleAuth } from "../components/auth/GoogleAuth";
 
 export const Register = () => {
   return (
     <div>
+      Register
+      <form>
+        <Input label="First Name" placeholder="First Name" />
+        <Input label="Last Name" placeholder="Last Name" />
+        <Input type="email" label="Email" placeholder="Email" />
+      </form>
       <Register />
       <GoogleAuth />
-      Register
     </div>
   );
 };
